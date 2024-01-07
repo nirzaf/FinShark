f5dce59c896bbac31c967247856d95d91f6b30f4
nirzaf <mfmfazrin1986@gmail.com>
2024-01-07 18:40:44 +0300

    Remove App import from index.tsx
    The App import has been removed from the main index.tsx file as it was an unnecessary inclusion. As well as this, the argument within the searchCompanies function call in a commented section of the code has been updated from "tsla" to "tesla".

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { router } from "./Routes/Routes";
 
@@ -10,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-//console.log(searchCompanies("tsla"));
+//console.log(searchCompanies("tesla"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
